350dd18cc14ecb6ddcf59cba993df11f1d1c8f5c
Sumanpreet SIngh <ssingh28@rrc.ca>
2026-03-12 15:13:25 -0400

    added health check endpoint

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,11 @@
 import express, { Express } from "express";
+import healthRoutes from "./api/v1/routes/healthRoutes";
 
 // Initialize Express application
 const app: Express = express();
 
-// Define a route
-app.get("/", (req, res) => {
-    res.send("Hello, World!");
-});
+app.use(express.json());
+
+app.use("/api/v1", healthRoutes);
 
 export default app;
