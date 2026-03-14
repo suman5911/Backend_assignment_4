5ce2183a6654672a4dcb96db2962424db8d1cb31
Sumanpreet SIngh <ssingh28@rrc.ca>
2026-03-14 10:24:15 -0400

    updated app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,30 @@
 import express, { Express } from "express";
+import { accessLogger , errorLogger , consoleLogger } from "./api/v1/middleware/logger";
+import errorHandler from "./api/v1/middleware/errorHandler";
 import healthRoutes from "./api/v1/routes/healthRoutes";
+import loanRoutes from "./api/v1/routes/loanRoutes";
+import authRoutes from "./api/v1/routes/authRoutes";
+import userRoutes from "./api/v1/routes/userRoutes";
+import adminRoutes from "./api/v1/routes/adminRoutes";
 
-// Initialize Express application
 const app: Express = express();
+
+if (process.env.NODE_ENV === "production") {
+    app.use(accessLogger);
+    app.use(errorLogger);
+} else {
+    app.use(consoleLogger);
+}
 
 app.use(express.json());
 
+// API Routes
 app.use("/api/v1", healthRoutes);
+app.use("/api/v1", loanRoutes);
+app.use("/api/v1", authRoutes);
+app.use("/api/v1/users", userRoutes);
+app.use("/api/v1/admin", adminRoutes);
+
+app.use(errorHandler);
 
 export default app;
